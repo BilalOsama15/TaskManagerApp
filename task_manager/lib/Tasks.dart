8dc133f70ef51e14cadbdc99d906eaf3a3e33dcf
BilalOsama15 <121038128+BilalOsama15@users.noreply.github.com>
2023-10-16 12:21:19 +0500

    Task Manager App Listing Design
    - Design Listing of task

---
+++ task_manager/lib/Tasks.dart
@@ -0,0 +1,70 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+class taskListing extends StatefulWidget {
+  const taskListing({super.key});
+
+  @override
+  State<taskListing> createState() => _taskListingState();
+}
+
+class _taskListingState extends State<taskListing> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 232, 219, 254),
+      appBar: AppBar(title: const Text("Tasks",style: TextStyle(color: Colors.white),),centerTitle: true,backgroundColor: Colors.deepPurple,),
+      body:  Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          20.height,
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 70,
+                  decoration: BoxDecoration(
+                    boxShadow: const [
+                      BoxShadow(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      offset: Offset(4,4),
+                      )
+                    ],
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color.fromARGB(255, 255, 255, 255)
+                  ),
+                  child: ListTile(
+                    title: Text("Task ${index+1}",),
+                    subtitle: Text("Due Date: 09/12/2023"),
+                    trailing: Icon(Icons.access_time_filled),
+                  ),
+                ),
+              );
+            
+              
+            },))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add Task',
+        onPressed: () { 
+       
+         },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+extension emptySpace on num
+{
+
+SizedBox get height => SizedBox(height:toDouble());
+
+SizedBox get width => SizedBox(width:toDouble());
+
+}
